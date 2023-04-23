69b06ad3ec282b1b75e5669f9f7c33a825895f9b
Maciej Fec <maciejfec1996@gmail.com>
2023-04-23 17:42:55 +0100

    add mappings

--- src/main/java/com/example/springbootpractice/controllers/BooksController.java
+++ src/main/java/com/example/springbootpractice/controllers/BooksController.java
@@ -1,19 +1,40 @@
 package com.example.springbootpractice.controllers;
 
 import com.example.springbootpractice.models.Book;
+import com.example.springbootpractice.services.BooksService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "api/v1/books")
 public class BooksController {
 
+    private final BooksService booksService;
+
+    public BooksController(BooksService booksService) {
+        this.booksService = booksService;
+    }
+
     @GetMapping
     public ResponseEntity<List<Book>> getAllBooks() {
+        return booksService.getAllBooks();
+    }
 
+    @GetMapping("{id}")
+    public ResponseEntity<Object> getBookById(@PathVariable("id") String bookId) {
+        return booksService.getBookById(bookId);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> addNewBook(@RequestBody Map<String, String> reqBody) {
+        return booksService.addNewBook(reqBody);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Object> deleteBook(@PathVariable("id") String bookId) {
+        return booksService.deleteBook(bookId);
     }
 }
